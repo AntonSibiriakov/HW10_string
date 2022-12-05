1cd331295a5694f160ac6b386c3e8ef07c59c570
ThinkPad <Tonan041@gmail.com>
2022-12-05 15:26:32 +0100

    Готовое ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,49 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("\n=====Task1=====");
+
+        String firstNme = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstNme + " " + middleName;
+
+        System.out.println(fullName);
+
+
+        System.out.println("\n=====Task2=====");
+
+        String fullNameUp="" + fullName.charAt(0);
+
+        for (int i = 1; i < fullName.length(); i++) {
+            if (fullName.charAt(i) == ' '){
+
+                fullNameUp+=' ';
+
+                fullNameUp += fullName.charAt(i+1);
+                i++;
+                continue;
+            }
+            char a = fullName.charAt(i);
+            a -= 32;
+            fullNameUp+=a;
+        }
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUp);
+
+
+        System.out.println("\n=====Task3=====");
+
+        String fullName3 ="Иванов Семён Семёнович";
+
+        fullName3 = fullName3.replace('ё','е');
+
+        System.out.println(fullName3);
+
+
+
 
     }
+
 }
+
+
